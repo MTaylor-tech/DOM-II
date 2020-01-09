afad47c62020e0f8166a494dad3cd197aa35bc6a
Matthew Taylor <matt@mtaylor.tech>
2020-01-08 21:58:44 -0500

    added keydown and wheel listeners

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 // Your code goes here
 
 // Selectors and variables
-const allImages = document.querySelectorAll('.img-content img');
-const allImageBoxes = document.querySelectorAll('.img-content');
+const allImages = document.querySelectorAll('img');
+const contentImages = document.querySelectorAll('.img-content img');
+const contentImageBoxes = document.querySelectorAll('.img-content');
 const firstImage = document.getElementById('fb-img');
 const navItems = document.querySelectorAll('.nav-link');
 const buttons = document.querySelectorAll('.btn');
+let fullSize = true;
 
 // Functions
 function fadeIn () {
@@ -17,12 +19,12 @@ function fadeOut () {
 }
 
 function loaded () {
-    gsap.to(allImages[0], {duration: 0, x: 1000});
-    gsap.to(allImages[1], {duration: 0, x: -1000});
+    gsap.to(contentImages[0], {duration: 0, x: 1000});
+    gsap.to(contentImages[1], {duration: 0, x: -1000});
 }
 
 function returnImgs (index) {
-    gsap.to(allImages[index], {duration: 2, x: 0});
+    gsap.to(contentImages[index], {duration: 2, x: 0});
 }
 
 function pressButton () {
@@ -35,12 +37,26 @@ function fontChange () {
 function fontChangeBack () {
     navItems.forEach(function (item) { item.style.color = 'black'; });
 }
+function changeFirstImage() {
+    if (fullSize) {
+        gsap.to(firstImage, {duration: 2, scale: 0.2, ease:'bounce'});
+        fullSize = false;
+    } else {
+        gsap.to(firstImage, {duration: 2, scale: 1, ease:'bounce'});
+        fullSize = true;
+    }
+}
+function rotateImages () {
+    allImages.forEach(function (image) { gsap.to(image, {duration: 1, rotation: 360}); });
+}
 
 // Add Event Listeners
 firstImage.addEventListener('mouseout', fadeIn);
 firstImage.addEventListener('mouseover', fadeOut);
+window.addEventListener('keydown', changeFirstImage);
 navItems.forEach(function (item) { item.addEventListener('mouseenter', fontChange); });
 navItems.forEach(function (item) { item.addEventListener('mouseleave', fontChangeBack); });
 buttons.forEach(function (button) { button.addEventListener('click', pressButton); });
 window.addEventListener('load', loaded);
-allImageBoxes.forEach(function (box, index) { box.addEventListener('mouseover', returnImgs.bind(null, index)); }, false);
+contentImageBoxes.forEach(function (box, index) { box.addEventListener('mouseover', returnImgs.bind(null, index)); }, false);
+window.addEventListener('wheel', rotateImages);
